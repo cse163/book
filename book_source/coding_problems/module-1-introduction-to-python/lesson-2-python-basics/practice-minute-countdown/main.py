c798803d9c8793a8f6b8427402822b90844ef619
Hunter Schafer <hschafer@uw.edu>
2021-08-25 23:56:10 -0700

    Add tests for previous input/output only tests

--- book_source/coding_problems/module-1-introduction-to-python/lesson-2-python-basics/practice-minute-countdown/main.py
+++ book_source/coding_problems/module-1-introduction-to-python/lesson-2-python-basics/practice-minute-countdown/main.py
@@ -1,6 +1,6 @@
 def main():
     # Write your code here!
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- book_source/coding_problems/module-2-data-structures-and-files/lesson-4-lists-and-files/practice-filter-long-lines/main.py
+++ book_source/coding_problems/module-2-data-structures-and-files/lesson-4-lists-and-files/practice-filter-long-lines/main.py
@@ -1,15 +1,14 @@
 def filter_long_lines(file_name, min_words):
     """
-    Prints each line in the file with the given name that has 
+    Prints each line in the file with the given name that has
     at least the given number of words in that line.
     """
-    # Write your code here :) 
-                
+    # Write your code here :)
 
-                
+
 def main():
-    filter_long_lines('song.txt', 7)
-    
+    filter_long_lines("song.txt", 7)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
