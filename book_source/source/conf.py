d2df9f443a0d2c2bb04e25d6f5c69412f17ce347
Hunter Schafer <hschafer@uw.edu>
2021-08-29 14:40:14 -0700

    Fix CSS Width and Spacing (#46)
    * Fix page width CSS
    
    When including `sphin-panels` they included new global CSS to make page
    smaller
    
    * Fix icon padding in prev/next buttons

--- book_source/source/conf.py
+++ book_source/source/conf.py
@@ -59,6 +59,8 @@ html_theme = "sphinx_book_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+
 
 html_title = project
 html_logo = "_static/logo.png"
